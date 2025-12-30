af221da9bbf98763a93835b1582b04ceb2e08043
Omay238 <238maculo238@gmail.com>
2025-12-30 12:23:20 -0700

    major refactor

--- src/lib.rs
+++ src/lib.rs
@@ -4,59 +4,74 @@
 //!
 //! utilities for generating netpbm images
 
-/// # Portable BitMap File
+/// NetPBMSaver
 ///
-/// creatable BitMap file.
-pub struct PBMFile {
+/// implements to_ascii and to_raw for saving
+pub trait NetPBMSaver {
+    /// create a text representation of the image file. an optional comment in the header.
+    fn to_ascii(&self, comment: Option<&str>) -> String;
+    /// create a binary representation of the image file.
+    fn to_raw(&self) -> Vec<u8>;
+}
+
+/// universal type for all netpbm files
+pub struct NetPBM<Class: NetPBMSaver> {
+    class: Class,
+}
+
+/// type for NetPBM files
+pub struct NetPBMFile {
     width: usize,
     height: usize,
     pixels: Vec<Vec<bool>>,
 }
 
-impl PBMFile {
-    /// create a new PBM File.
-    ///
-    /// - width      - immutable size for image width
-    /// - height     - immutable size for image height
-    /// - background - default background color. false is white, true is black.
-    pub fn new(width: usize, height: usize, background: bool) -> Self {
-        Self {
-            width,
-            height,
-            pixels: vec![vec![background; width]; height],
-        }
-    }
+/// type for NetPGM files
+pub struct NetPGMFile {
+    width: usize,
+    height: usize,
+    pixels: Vec<Vec<u8>>,
+}
 
-    /// set a pixels value
-    ///
-    /// - x     - x position of pixel. does nothing if not in image.
-    /// - y     - y position of pixel. does nothing if not in image.
-    /// - value - value of pixel. false is white, true is black.
-    pub fn set_pixel(&mut self, x: usize, y: usize, value: bool) {
-        if x < self.width && y < self.height {
-            self.pixels[y][x] = value;
-        }
-    }
+/// type for NetPPM files
+pub struct NetPPMFile {
+    width: usize,
+    height: usize,
+    pixels: Vec<Vec<[u8; 3]>>,
+}
 
-    /// get a pixels value
-    ///
-    /// - x     - x position of pixel. does nothing if not in image.
-    /// - y     - y position of pixel. does nothing if not in image.
-    ///
-    /// returns - value of pixel. false is white, true is black.
-    pub fn get_pixel(&mut self, x: usize, y: usize) -> Option<bool> {
-        if x < self.width && y < self.height {
-            return Some(self.pixels[y][x]);
-        }
-        None
-    }
-
+impl<Class: NetPBMSaver> NetPBM<Class> {
     /// convert the image to its ASCII representation
     ///
     /// - comment - optional value to add a comment in the header.
     ///
     /// returns - ASCII representation of the image
     pub fn to_ascii(&self, comment: Option<&str>) -> String {
+        self.class.to_ascii(comment)
+    }
+
+    /// convert the image to its binary representation
+    ///
+    /// returns - binary representation of the image
+    pub fn to_raw(&self) -> Vec<u8> {
+        self.class.to_raw()
+    }
+
+    /// save the image in its ASCII representation
+    pub fn save_ascii(&self, path: &str, comment: Option<&str>) -> std::io::Result<()> {
+        std::fs::write(path, self.class.to_ascii(comment))?;
+        Ok(())
+    }
+
+    /// save the image in its binary representation
+    pub fn save_raw(&self, path: &str) -> std::io::Result<()> {
+        std::fs::write(path, self.class.to_raw())?;
+        Ok(())
+    }
+}
+
+impl NetPBMSaver for NetPBMFile {
+    fn to_ascii(&self, comment: Option<&str>) -> String {
         let mut comment_text = String::new();
         if let Some(comment) = comment {
             comment_text = format!("\n# {}", comment.replace("\n", "\n# "));
@@ -79,10 +94,7 @@ impl PBMFile {
         )
     }
 
-    /// convert the image to its binary representation
-    ///
-    /// returns - binary representation of the image
-    pub fn to_raw(&self) -> Vec<u8> {
+    fn to_raw(&self) -> Vec<u8> {
         let mut bits = Vec::new();
         for (row_id, row) in self.pixels.iter().enumerate() {
             for (i, v) in row.to_vec().iter().enumerate() {
@@ -103,73 +115,10 @@ impl PBMFile {
         ]
         .concat()
     }
-
-    /// save the image in its ASCII representation
-    pub fn save_ascii(&self, path: &str, comment: Option<&str>) -> std::io::Result<()> {
-        std::fs::write(path, self.to_ascii(comment))?;
-        Ok(())
-    }
-
-    /// save the image in its binary representation
-    pub fn save_raw(&self, path: &str) -> std::io::Result<()> {
-        std::fs::write(path, self.to_raw())?;
-        Ok(())
-    }
 }
 
-/// # Portable GrayMap File
-///
-/// creatable GrayMap file.
-pub struct PGMFile {
-    width: usize,
-    height: usize,
-    pixels: Vec<Vec<u8>>,
-}
-
-impl PGMFile {
-    /// create a new PGM File.
-    ///
-    /// - width      - immutable size for image width
-    /// - height     - immutable size for image height
-    /// - background - default background color. 0 is black, 255 is white.
-    pub fn new(width: usize, height: usize, background: u8) -> Self {
-        Self {
-            width,
-            height,
-            pixels: vec![vec![background; width]; height],
-        }
-    }
-
-    /// set a pixels value
-    ///
-    /// - x     - x position of pixel. does nothing if not in image.
-    /// - y     - y position of pixel. does nothing if not in image.
-    /// - value - value of pixel. 0 is black, 255 is white.
-    pub fn set_pixel(&mut self, x: usize, y: usize, value: u8) {
-        if x < self.width && y < self.height {
-            self.pixels[y][x] = value;
-        }
-    }
-
-    /// get a pixels value
-    ///
-    /// - x     - x position of pixel. does nothing if not in image.
-    /// - y     - y position of pixel. does nothing if not in image.
-    ///
-    /// returns - value of pixel. 0 is black, 255 is white.
-    pub fn get_pixel(&mut self, x: usize, y: usize) -> Option<u8> {
-        if x < self.width && y < self.height {
-            return Some(self.pixels[y][x]);
-        }
-        None
-    }
-
-    /// convert the image to its ASCII representation
-    ///
-    /// - comment - optional value to add a comment in the header.
-    ///
-    /// returns - ASCII representation of the image
-    pub fn to_ascii(&self, comment: Option<&str>) -> String {
+impl NetPBMSaver for NetPGMFile {
+    fn to_ascii(&self, comment: Option<&str>) -> String {
         let mut comment_text = String::new();
         if let Some(comment) = comment {
             comment_text = format!("\n# {}", comment.replace("\n", "\n# "));
@@ -192,10 +141,7 @@ impl PGMFile {
         )
     }
 
-    /// convert the image to its binary representation
-    ///
-    /// returns - binary representation of the image
-    pub fn to_raw(&self) -> Vec<u8> {
+    fn to_raw(&self) -> Vec<u8> {
         [
             &[80, 53, 10], // P5\n
             format!("{}", self.width).as_bytes(),
@@ -206,73 +152,10 @@ impl PGMFile {
         ]
         .concat()
     }
-
-    /// save the image in its ASCII representation
-    pub fn save_ascii(&self, path: &str, comment: Option<&str>) -> std::io::Result<()> {
-        std::fs::write(path, self.to_ascii(comment))?;
-        Ok(())
-    }
-
-    /// save the image in its binary representation
-    pub fn save_raw(&self, path: &str) -> std::io::Result<()> {
-        std::fs::write(path, self.to_raw())?;
-        Ok(())
-    }
 }
 
-/// # Portable PixMap File
-///
-/// creatable PixMap file.
-pub struct PPMFile {
-    width: usize,
-    height: usize,
-    pixels: Vec<Vec<[u8; 3]>>,
-}
-
-impl PPMFile {
-    /// create a new PPM File.
-    ///
-    /// - width      - immutable size for image width
-    /// - height     - immutable size for image height
-    /// - background - default background color. rgb order. 0 is black, 255 is white.
-    pub fn new(width: usize, height: usize, background: [u8; 3]) -> Self {
-        Self {
-            width,
-            height,
-            pixels: vec![vec![background; width]; height],
-        }
-    }
-
-    /// set a pixels color
-    ///
-    /// - x     - x position of pixel. does nothing if not in image.
-    /// - y     - y position of pixel. does nothing if not in image.
-    /// - color - color of pixel. rgb order. 0 is black, 255 is white.
-    pub fn set_pixel(&mut self, x: usize, y: usize, color: [u8; 3]) {
-        if x < self.width && y < self.height {
-            self.pixels[y][x] = color;
-        }
-    }
-
-    /// get a pixels color
-    ///
-    /// - x     - x position of pixel. does nothing if not in image.
-    /// - y     - y position of pixel. does nothing if not in image.
-    ///
-    /// returns - color of pixel. rgb order. 0 is black, 255 is white.
-    pub fn get_pixel(&mut self, x: usize, y: usize) -> Option<[u8; 3]> {
-        if x < self.width && y < self.height {
-            return Some(self.pixels[y][x]);
-        }
-        None
-    }
-
-    /// convert the image to its ASCII representation
-    ///
-    /// - comment - optional value to add a comment in the header.
-    ///
-    /// returns - ASCII representation of the image
-    pub fn to_ascii(&self, comment: Option<&str>) -> String {
+impl NetPBMSaver for NetPPMFile {
+    fn to_ascii(&self, comment: Option<&str>) -> String {
         let mut comment_text = String::new();
         if let Some(comment) = comment {
             comment_text = format!("\n# {}", comment.replace("\n", "\n# "));
@@ -295,10 +178,7 @@ impl PPMFile {
         )
     }
 
-    /// convert the image to its binary representation
-    ///
-    /// returns - binary representation of the image
-    pub fn to_raw(&self) -> Vec<u8> {
+    fn to_raw(&self) -> Vec<u8> {
         [
             &[80, 54, 10], // P6\n
             format!("{}", self.width).as_bytes(),
@@ -315,16 +195,124 @@ impl PPMFile {
         ]
         .concat()
     }
+}
 
-    /// save the image in its ASCII representation
-    pub fn save_ascii(&self, path: &str, comment: Option<&str>) -> std::io::Result<()> {
-        std::fs::write(path, self.to_ascii(comment))?;
-        Ok(())
+impl NetPBM<NetPBMFile> {
+    /// create a new PBM File.
+    ///
+    /// - width      - immutable size for image width
+    /// - height     - immutable size for image height
+    /// - background - default background color. false is white, true is black.
+    pub fn new_pbm(width: usize, height: usize, background: bool) -> Self {
+        let class = NetPBMFile {
+            width,
+            height,
+            pixels: vec![vec![background; width]; height],
+        };
+        Self { class }
     }
 
-    /// save the image in its binary representation
-    pub fn save_raw(&self, path: &str) -> std::io::Result<()> {
-        std::fs::write(path, self.to_raw())?;
-        Ok(())
+    /// set a pixels value
+    ///
+    /// - x     - x position of pixel. does nothing if not in image.
+    /// - y     - y position of pixel. does nothing if not in image.
+    /// - value - value of pixel. false is white, true is black.
+    pub fn set_pixel(&mut self, x: usize, y: usize, value: bool) {
+        if x < self.class.width && y < self.class.height {
+            self.class.pixels[y][x] = value;
+        }
+    }
+
+    /// get a pixels value
+    ///
+    /// - x     - x position of pixel. does nothing if not in image.
+    /// - y     - y position of pixel. does nothing if not in image.
+    ///
+    /// returns - value of pixel. false is white, true is black.
+    pub fn get_pixel(&mut self, x: usize, y: usize) -> Option<bool> {
+        if x < self.class.width && y < self.class.height {
+            return Some(self.class.pixels[y][x]);
+        }
+        None
+    }
+}
+
+impl NetPBM<NetPGMFile> {
+    /// create a new PGM File.
+    ///
+    /// - width      - immutable size for image width
+    /// - height     - immutable size for image height
+    /// - background - default background color. 0 is black, 255 is white.
+    pub fn new_pgm(width: usize, height: usize, background: u8) -> Self {
+        let class = NetPGMFile {
+            width,
+            height,
+            pixels: vec![vec![background; width]; height],
+        };
+        Self { class }
+    }
+
+    /// set a pixels value
+    ///
+    /// - x     - x position of pixel. does nothing if not in image.
+    /// - y     - y position of pixel. does nothing if not in image.
+    /// - value - value of pixel. 0 is black, 255 is white.
+    pub fn set_pixel(&mut self, x: usize, y: usize, value: u8) {
+        if x < self.class.width && y < self.class.height {
+            self.class.pixels[y][x] = value;
+        }
+    }
+
+    /// get a pixels value
+    ///
+    /// - x     - x position of pixel. does nothing if not in image.
+    /// - y     - y position of pixel. does nothing if not in image.
+    ///
+    /// returns - value of pixel. 0 is black, 255 is white.
+    pub fn get_pixel(&mut self, x: usize, y: usize) -> Option<u8> {
+        if x < self.class.width && y < self.class.height {
+            return Some(self.class.pixels[y][x]);
+        }
+        None
+    }
+}
+
+impl NetPBM<NetPPMFile> {
+    /// create a new PPM File.
+    ///
+    /// - width      - immutable size for image width
+    /// - height     - immutable size for image height
+    /// - background - default background color. rgb order. 0 is black, 255 is white.
+    pub fn new_ppm(width: usize, height: usize, background: [u8; 3]) -> Self {
+        let class = NetPPMFile {
+            width,
+            height,
+            pixels: vec![vec![background; width]; height],
+        };
+        Self { class }
+    }
+
+    /// set a pixels color
+    ///
+    /// - x     - x position of pixel. does nothing if not in image.
+    /// - y     - y position of pixel. does nothing if not in image.
+    /// - color - color of pixel. rgb order. 0 is black, 255 is white.
+    pub fn set_pixel(&mut self, x: usize, y: usize, color: [u8; 3]) {
+        if x < self.class.width && y < self.class.height {
+            self.class.pixels[y][x] = color;
+        }
+    }
+
+    /// get a pixels color
+    ///
+    /// - x     - x position of pixel. does nothing if not in image.
+    /// - y     - y position of pixel. does nothing if not in image.
+    ///
+    /// returns - color of pixel. rgb order. 0 is black, 255 is white.
+    pub fn get_pixel(&mut self, x: usize, y: usize) -> Option<[u8; 3]> {
+        if x < self.class.width && y < self.class.height {
+            return Some(self.class.pixels[y][x]);
+        }
+        None
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
-use libpbm::{PBMFile, PGMFile, PPMFile};
+use libpbm::NetPBM;
 
 fn main() {
-    let mut pbm = PBMFile::new(2, 2, false);
+    let mut pbm = NetPBM::new_pbm(2, 2, false);
     pbm.set_pixel(0, 0, false);
     pbm.set_pixel(1, 0, true);
     pbm.set_pixel(0, 1, true);
@@ -9,7 +9,7 @@ fn main() {
     pbm.save_ascii("ascii.pbm", None).unwrap();
     pbm.save_raw("raw.pbm").unwrap();
 
-    let mut pgm = PGMFile::new(4, 2, 255);
+    let mut pgm = NetPBM::new_pgm(4, 2, 255);
     pgm.set_pixel(0, 0, 0);
     pgm.set_pixel(1, 0, 32);
     pgm.set_pixel(2, 0, 64);
@@ -21,7 +21,7 @@ fn main() {
     pgm.save_ascii("ascii.pgm", None).unwrap();
     pgm.save_raw("raw.pgm").unwrap();
 
-    let mut ppm = PPMFile::new(4, 2, [255, 255, 255]);
+    let mut ppm = NetPBM::new_ppm(4, 2, [255, 255, 255]);
     ppm.set_pixel(0, 0, [0, 0, 0]);
     ppm.set_pixel(1, 0, [0, 0, 255]);
     ppm.set_pixel(2, 0, [0, 255, 0]);
@@ -34,7 +34,7 @@ fn main() {
     ppm.save_raw("raw.ppm").unwrap();
 
     let s = 512;
-    let mut big_ppm = PPMFile::new(s, s, [255, 255, 255]);
+    let mut big_ppm = NetPBM::new_ppm(s, s, [255, 255, 255]);
     for x in 0..s {
         for y in 0..s {
             let dx = x as i32 - (s / 2) as i32;
@@ -54,25 +54,31 @@ fn main() {
 
             let color_1;
 
-            if 0.0 <= hue_prime && hue_prime < 1.0 {
+            if (0.0..1.0).contains(&hue_prime) {
                 color_1 = [chroma, intermediate, 0.0];
-            } else if 1.0 <= hue_prime && hue_prime < 2.0 {
+            } else if (1.0..2.0).contains(&hue_prime) {
                 color_1 = [intermediate, chroma, 0.0];
-            } else if 2.0 <= hue_prime && hue_prime < 3.0 {
+            } else if (2.0..3.0).contains(&hue_prime) {
                 color_1 = [0.0, chroma, intermediate];
-            } else if 3.0 <= hue_prime && hue_prime < 4.0 {
+            } else if (3.0..4.0).contains(&hue_prime) {
                 color_1 = [0.0, intermediate, chroma];
-            } else if 4.0 <= hue_prime && hue_prime < 5.0 {
+            } else if (4.0..5.0).contains(&hue_prime) {
                 color_1 = [intermediate, 0.0, chroma];
             } else {
                 color_1 = [chroma, 0.0, intermediate];
             }
 
             let m = value - chroma;
-            let color = [((color_1[0] + m) * 255.0) as u8, ((color_1[1] + m) * 255.0) as u8, ((color_1[2] + m) * 255.0) as u8];
+            let color = [
+                ((color_1[0] + m) * 255.0) as u8,
+                ((color_1[1] + m) * 255.0) as u8,
+                ((color_1[2] + m) * 255.0) as u8,
+            ];
 
             big_ppm.set_pixel(x, y, color);
         }
     }
-    big_ppm.save_ascii("big.ppm", Some("wow, this is a big image!")).unwrap();
+    big_ppm
+        .save_ascii("big.ppm", Some("wow, this is a big image!"))
+        .unwrap();
 }
